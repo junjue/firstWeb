8618e9bf98e3e1a329e70cdc10bcc0b24ea8ff74
junjue <junjuewang@gmail.com>
2014-11-01 15:46:36 -0400

    finished 06/003_likebutton

--- experiments/js/likebutton.js
+++ experiments/js/likebutton.js
@@ -2,42 +2,36 @@
 
     function init() {
         $("#likeButton").click(likecheckHandler);
-        $("#unlikeButton").click(unlikecheckHandler);
-       
     }
 
+    function checkimg() {
+
+        if ($("#likeButton").hasClass("liked") == true) {
+            document.getElementById("likeButton").src = "../../images/image/liked2.png";
+        }
+        else
+            document.getElementById("likeButton").src = "../../images/image/like.png";
+    }
+
+    function checkstatus(mark) {
+        $("#likestatus").empty();
+        $("#likestatus").append(mark);
+    }
 
     function likecheckHandler() {
-        var likemark = $("input[id='likeButton']:checked").val();
-       // while ($("input[id='unlikeButton']:checked").val() == null) {
-            if (likemark == "on") {
-                var mark = "Added to liked";
-                $("#likestatus").empty();
-                $("#likestatus").append(mark);
-            }
-            else {
-                var mark = "removed from liked";
-                $("#likestatus").empty();
-                $("#likestatus").append(mark);
-            }
 
-    }
-
-    function unlikecheckHandler() {
-        var likemark = $("input[id='unlikeButton']:checked").val();
-        if (likemark == "on") {
-            var mark = "disliked";
-            $("#likestatus").empty();
-            $("#likestatus").append(mark);
+        if ($("#likeButton").hasClass("liked") == false) {
+            var mark = "Liked<br/>Thank you<br/>You could cancel your like by clicking again";
+            $("#likeButton").addClass("liked");
+            checkstatus(mark);
         }
         else {
-            var mark = "cancel disliked";
-            $("#likestatus").empty();
-            $("#likestatus").append(mark);
+            var mark = "removed like<br/>You could relike by clicking the button";
+            $("#likeButton").removeClass("liked");
+            checkstatus(mark);
         }
-
+        checkimg();
     }
-
 
     init();
 });
